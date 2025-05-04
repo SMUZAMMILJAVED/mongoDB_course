6e344fac583928cde8753fc620dc519a79c70a16
S MUZAMMIL JAVED <s.muzammiljaved@gmail.com>
2025-05-04 05:15:29 +0500

    update

--- mongo_02/models/connection.js
+++ mongo_02/models/connection.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/myDataBase');
+mongoose.connect('mongodb+srv://test:test%40123@cluster0.obzkur0.mongodb.net/')
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 module.exports=mongoose;
